ebc512dca3b3ffd67332f8bb213288e38a0c9c42
Vinicius Lima da Mata <viniciuslimadamata@gmail.com>
2020-08-26 16:56:24 -0400

    servindo o front buildado

---
+++ public/precache-manifest.2c2d557c4278aa497966a2d6cf4c442b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca28b8f4fd2abb3ee18867eeff72d6e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3273006c1ca3c7362843",
+    "url": "/static/js/2.01269bd7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.01269bd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95ddb835965b85b667ef",
+    "url": "/static/js/main.68984bb8.chunk.js"
+  },
+  {
+    "revision": "658fd86e25164571bd81",
+    "url": "/static/js/runtime-main.977811b3.js"
+  },
+  {
+    "revision": "787c6af98ac770e43e65815a8ff337df",
+    "url": "/static/media/camera.787c6af9.svg"
+  },
+  {
+    "revision": "e2a499354e3b1d309ff71584d7290d29",
+    "url": "/static/media/comment.e2a49935.svg"
+  },
+  {
+    "revision": "00e13584091199bddd3785adaf0b9f3d",
+    "url": "/static/media/like.00e13584.svg"
+  },
+  {
+    "revision": "83fd0bcc6cdc21e671ae5edf856ba3ef",
+    "url": "/static/media/logo.83fd0bcc.svg"
+  },
+  {
+    "revision": "22d960a9ac06cc252579791795bdea8c",
+    "url": "/static/media/more.22d960a9.svg"
+  },
+  {
+    "revision": "adb6223208cf4c30490619b7e72b5635",
+    "url": "/static/media/profile-picture.adb62232.png"
+  },
+  {
+    "revision": "55e74cfdc1470a74c5bc8c2af6ed9fa0",
+    "url": "/static/media/send.55e74cfd.svg"
+  }
+]);
